153dea460c8feecdac50dc3c1e7e5752b81dd1ea
Sergey Berezin <sergey@itis.cs.msu.ru>
2020-09-05 23:27:55 +0300

    Computer vision sample app, gitignore and no license

---
+++ AzureComputerVision/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+
+namespace AzureComputerVision
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var key = Environment.GetEnvironmentVariable("COMPUTER_VISION_API_KEY");
+            var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+                { Endpoint = "https://visiondemosb.cognitiveservices.azure.com/" };
+            
+            using var imageStream = File.OpenRead(args.FirstOrDefault() ?? "sample.jpg");
+            var result = await client.DetectObjectsInStreamAsync(imageStream);
+          
+            foreach(var obj in result.Objects)
+            {
+                Console.WriteLine($"{obj.ObjectProperty} with confidence {obj.Confidence}");
+            }
+        }
+    }
+}
