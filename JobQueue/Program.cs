fb4a14a8028c688c4bc4b75ff9e01589491c04da
Sergey Berezin <sergey@itis.cs.msu.ru>
2020-10-01 05:59:21 +0300

    Job queue sample

---
+++ JobQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace jobqueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentQueue<int> jobs = new ConcurrentQueue<int>();
+
+            int numProcs = Environment.ProcessorCount;
+            
+            for(int i = 0;i<numProcs;i++)
+                new Thread(() => {
+                    while(true) {
+                        if(jobs.TryDequeue(out int job)) {
+                            Console.WriteLine($"{job}");
+                        } else
+                            Thread.Sleep(0);
+                    }
+                }).Start();
+
+            for(int i = 0;i<100;i++)
+                jobs.Enqueue(i);
+        }
+    }
+}
