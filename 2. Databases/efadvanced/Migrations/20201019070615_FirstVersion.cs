9cce75cdf53a89fa5c5576c5a7aa4037e8a25202
Sergey Berezin <sergey@itis.cs.msu.ru>
2020-10-26 10:55:14 +0300

    Advanced EF sample

---
+++ 2. Databases/efadvanced/Migrations/20201019070615_FirstVersion.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace efsample.Migrations
+{
+    public partial class FirstVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
---
+++ 2. Databases/efadvanced/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace efsample
+{
+    public class Author 
+    {
+        public int AuthorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        virtual public ICollection<Book> Books { get; set; }
+
+        public byte[] Photo { get; set; }
+    }
+
+    public class Book 
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int Pages { get; set; }
+        virtual public ICollection<Author> Authors { get; set; }
+    }
+
+    class LibraryContext : DbContext 
+    {
+        public DbSet<Author> Authors { get; set; }   
+        public DbSet<Book> Books { get; set; } 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder o) 
+            => o.UseLazyLoadingProxies(). UseSqlite("Data Source=library.db");
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db = new LibraryContext()) 
+            {
+                var a = db.Authors.Where(a => a.FirstName.StartsWith("F")).FirstOrDefault();
+                var b = new Book() { Title = "Dune", Pages = 450 };
+                b.Authors = new List<Author>();
+                b.Authors.Add(a);
+                a.Books = new List<Book>();
+                a.Books.Add(b);
+                db.Add(b);
+
+                db.SaveChanges();
+
+                // db.Add(new Author { FirstName = "John", LastName = "Smith"});
+                // db.SaveChanges();
+
+                foreach(var a2 in db.Authors/*.Include(a => a.Books)*/)
+                {
+                    Console.WriteLine($"{a2.AuthorId} {a2.FirstName} {a2.LastName}");
+                    // db.Entry(a2).Collection(a => a.Books).Load();
+                    foreach(var b2 in a2.Books)
+                         Console.WriteLine($"- {b2.BookId} {b2.Title} {b2.Pages}");
+                }
+            }
+        }
+    }
+}
