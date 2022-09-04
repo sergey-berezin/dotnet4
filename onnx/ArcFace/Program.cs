406f5790138801ff244280e3cbd8aa05ec40133a
Sergey Berezin <sergey@itis.cs.msu.ru>
2022-09-04 16:49:53 +0300

    ONNX samples updated

---
+++ onnx/ArcFace/Program.cs
@@ -0,0 +1,70 @@
+﻿
+// Download ONNX model from https://github.com/onnx/models/blob/main/vision/body_analysis/arcface/model/arcfaceresnet100-8.onnx
+// to project directory before run
+
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+using var session = new InferenceSession("arcfaceresnet100-8.onnx");  
+Console.WriteLine("Predicting contents of image..."); 
+
+foreach(var kv in session.InputMetadata)
+    Console.WriteLine($"{kv.Key}: {MetadataToString(kv.Value)}");
+foreach(var kv in session.OutputMetadata)
+    Console.WriteLine($"{kv.Key}: {MetadataToString(kv.Value)}]");
+
+using var face1 = Image.Load<Rgb24>("face1.png");
+using var face2 = Image.Load<Rgb24>("face2.png");
+
+var embeddings1 = GetEmbeddings(face1);
+var embeddings2 = GetEmbeddings(face2);
+
+Console.WriteLine($"Distance =  {Distance(embeddings1, embeddings2) * Distance(embeddings1, embeddings2)}");
+Console.WriteLine($"Similarity =  {Similarity(embeddings1, embeddings2)}");
+
+string MetadataToString(NodeMetadata metadata)
+    => $"{metadata.ElementType}[{String.Join(",", metadata.Dimensions.Select(i => i.ToString()))}]";
+
+float Length(float[] v) => (float)Math.Sqrt(v.Select(x => x*x).Sum());
+
+float[] Normalize(float[] v) 
+{
+    var len = Length(v);
+    return v.Select(x => x / len).ToArray();
+}
+
+float Distance(float[] v1, float[] v2) => Length(v1.Zip(v2).Select(p => p.First - p.Second).ToArray());
+
+float Similarity(float[] v1, float[] v2) => v1.Zip(v2).Select(p => p.First * p.Second).Sum();
+
+DenseTensor<float> ImageToTensor(Image<Rgb24> img)
+{
+    var w = img.Width;
+    var h = img.Height;
+    var t = new DenseTensor<float>(new[] { 1, 3, h, w });
+
+    img.ProcessPixelRows(pa => 
+    {
+        for (int y = 0; y < h; y++)
+        {           
+            Span<Rgb24> pixelSpan = pa.GetRowSpan(y);
+            for (int x = 0; x < w; x++)
+            {
+                t[0, 0, y, x] = pixelSpan[x].R;
+                t[0, 1, y, x] = pixelSpan[x].G;
+                t[0, 2, y, x] = pixelSpan[x].B;
+            }
+        }
+    });
+    
+    return t;
+}
+
+float[] GetEmbeddings(Image<Rgb24> face) 
+{
+    var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("data", ImageToTensor(face)) };
+    using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
+    return Normalize(results.First(v => v.Name == "fc1").AsEnumerable<float>().ToArray());
+}
---
+++ onnx/EmotionFerPlus/Program.cs
@@ -0,0 +1,61 @@
+﻿// Download ONNX model from https://github.com/onnx/models/blob/main/vision/body_analysis/emotion_ferplus/model/emotion-ferplus-7.onnx
+// to project directory before build
+
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+using var session = new InferenceSession("emotion-ferplus-7.onnx");  
+
+foreach(var kv in session.InputMetadata)
+    Console.WriteLine($"{kv.Key}: {MetadataToString(kv.Value)}");
+foreach(var kv in session.OutputMetadata)
+    Console.WriteLine($"{kv.Key}: {MetadataToString(kv.Value)}]");
+
+using Image<Rgb24> image = Image.Load<Rgb24>("face1.png");
+image.Mutate(ctx => {
+    ctx.Resize(new Size(64,64));
+   // ctx.Grayscale();
+});
+
+var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("Input3", GrayscaleImageToTensor(image)) };
+using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
+var emotions = Softmax(results.First(v => v.Name == "Plus692_Output_0").AsEnumerable<float>().ToArray());
+
+string[] keys = { "neutral", "happiness", "surprise", "sadness", "anger", "disgust", "fear", "contempt" };
+foreach(var i in keys.Zip(emotions))
+    Console.WriteLine($"{i.First}: {i.Second}");
+
+
+DenseTensor<float> GrayscaleImageToTensor(Image<Rgb24> img)
+{
+    var w = img.Width;
+    var h = img.Height;
+    var t = new DenseTensor<float>(new[] { 1, 1, h, w });
+
+    img.ProcessPixelRows(pa => 
+    {
+        for (int y = 0; y < h; y++)
+        {           
+            Span<Rgb24> pixelSpan = pa.GetRowSpan(y);
+            for (int x = 0; x < w; x++)
+            {
+                t[0, 0, y, x] = pixelSpan[x].R; // B and G are the same
+            }
+        }
+    });
+    
+    return t;
+}
+
+string MetadataToString(NodeMetadata metadata)
+    => $"{metadata.ElementType}[{String.Join(",", metadata.Dimensions.Select(i => i.ToString()))}]";
+
+float[] Softmax(float[] z)
+ {
+    var exps = z.Select(x => Math.Exp(x)).ToArray();
+    var sum = exps.Sum();
+    return exps.Select(x => (float)(x / sum)).ToArray();
+}
