b2deda76fcd1e69646b2d28e96620195bb33ef2d
Sergey Berezin <sergey@itis.cs.msu.ru>
2023-10-07 09:28:30 +0300

    More WPF samples

---
+++ AsyncCommand/AsyncCommand/AsyncRelayCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AsyncCommand
+{
+    public class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<object, bool> canExecute;
+        private readonly Func<object, Task> executeAsync;
+        private bool isExecuting;
+
+        public AsyncRelayCommand(Func<object, Task> executeAsync, Func<object, bool> canExecute = null)
+        {
+            this.executeAsync = executeAsync;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (isExecuting)
+            {
+                return false;
+            }
+            else
+            {
+                return canExecute is null || canExecute(parameter);
+            }
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (!isExecuting)
+            {
+                isExecuting = true;
+                executeAsync(parameter).ContinueWith(_ =>
+                {
+                    isExecuting = false;
+                    CommandManager.InvalidateRequerySuggested();
+                }, scheduler: TaskScheduler.FromCurrentSynchronizationContext());
+            }
+        }
+    }
+}
---
+++ AsyncCommand/AsyncCommand/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncCommand
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ICommand startCommand;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            startCommand = new AsyncRelayCommand(async _ =>
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    label.Content = i;
+                    await Task.Delay(1000);
+                }
+            });
+
+            DataContext = this;
+        }
+
+        public ICommand StartCommand => startCommand;
+        
+    }
+}
