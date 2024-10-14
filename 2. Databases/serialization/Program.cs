59a30155278d92e3fd781ca8c28cb6710015256f
Sergey Berezin <b-sebere@microsoft.com>
2024-10-14 10:14:41 +0300

    Serialization sample

---
+++ 2. Databases/serialization/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+var x = new double[] { 2.87, 3.1415 };
+var y = new double[] { -1.0, 1.0 };
+
+var bytes = new byte[4 * sizeof(double)];
+Buffer.BlockCopy(x, 0, bytes, 0, 2 * sizeof(double));
+Buffer.BlockCopy(y, 0, bytes, 2 * sizeof(double), 2 * sizeof(double));
+
+foreach(var b in bytes)
+    Console.Write($"{b:X02}");
+Console.WriteLine();
+
+var l = new List {
+    x = 3.1415,
+    next = new List {
+        x = 2.87
+    }
+};
+string s = JsonConvert.SerializeObject(l);
+Console.WriteLine(s);
+
+l.next.next = l;
+string s2 = JsonConvert.SerializeObject(l, new JsonSerializerSettings {
+    ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+    PreserveReferencesHandling = PreserveReferencesHandling.Objects
+});
+
+
+class List
+{
+    public double x;
+    public List next;
+}
